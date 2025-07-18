61028ce60531b2adf2a3dc5af051a9e6e5f7fc44
schernyshov <sergiy.chernyshov@zalando.de>
2025-07-18 09:36:04 +0200

    chore: refactoring structure of the project slightly

---
+++ .github/workflows/variables.tf
@@ -0,0 +1,30 @@
+variable "image_uri" {
+  description = "The ECR image URI for the Lambda function"
+  type        = string
+  sensitive   = true
+}
+
+variable "aws_region" {
+  description = "AWS region to deploy resources in"
+  type        = string
+  default     = "eu-central-1"
+}
+
+variable "environment" {
+  description = "Deployment environment (e.g. dev, prod)"
+  type        = string
+  default     = "prod"
+}
+
+variable "lambda_function_name" {
+  description = "Name of the Lambda function"
+  type        = string
+  default     = "url-shortener"
+}
+
+variable "sns_email" {
+  description = "Email address for SNS alarm notifications"
+  type        = string
+  default     = "sergiy.chernyshow@gmail.com"  # Replace or override in tfvars or via CLI
+}
+
